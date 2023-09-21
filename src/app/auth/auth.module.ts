2625e64f574e24b11054a26294fe91f2c723a8bc
Mukut Prasad <mukutprasad1992@gmail.com>
2023-09-21 20:30:09 +0530

    changed logo

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,8 +5,9 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,12 +15,13 @@ import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.compone
     LoginComponent,
     RegisterComponent,
     ForgotpasswordComponent
-   ],
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ]
 })
 
--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -4,7 +4,19 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+// export class RegisterService {
+
+//   constructor(private http: HttpClient) { }
+
+//   registerData(data: any): Observable<any> {
+//     return this.http.post('http://localhost:3000/users/register', data);
+//   }
+// }
+
+// 3.110.193.224
+
 export class RegisterService {
+  private apiUrl = 'your_registration_api_url';
 
   constructor(private http: HttpClient) { }
 
@@ -12,5 +24,3 @@ export class RegisterService {
     return this.http.post('http://localhost:3000/users/register', data);
   }
 }
-
-// 3.110.193.224
